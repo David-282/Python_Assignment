87212cdb9c2b5a59709680b5a5ce926ca72c73c7
David-282 <afolabidavid94@gmail.com>
2026-02-19 09:18:16 +0100

    Assignment

---
+++ StudentManagement/src/student_management_system/student_creation.py
@@ -0,0 +1,78 @@
+
+
+class Student:
+    __no_of_student = 0
+    def __init__(self,name: str,age: int,phone_number: str,gender: str, email:str ):
+
+        self.__email = None
+        self.set_email(email)
+
+
+        Student.__no_of_student += 1
+        self.__student_id = 000 +(self.__no_of_student)
+
+        self.__age = None
+        self.set_age(age)
+
+        self.__phone_number = None
+        self.set_phone_number(phone_number)
+
+        self.__name = name
+        self.set_name(name)
+
+        self.__gender = None
+        self.set_gender(gender)
+
+
+
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
+    def get_email(self):
+        return self.__email
+
+    def get_phone_number(self):
+        return self.__phone_number
+
+    def get_gender(self):
+        return self.__gender
+
+    def get_student_id(self):
+        return self.__student_id
+
+    def set_name(self,name):
+        if name.strip() == "":
+            raise ValueError("Student name cannot be empty")
+
+        self.__name = name.strip()
+
+    def set_age(self,age):
+        if age <= 0:
+            raise ValueError("Age must be greater than 0")
+
+        self.__age = age
+
+    def set_gender(self,gender):
+        if gender is None or gender == "":
+            raise ValueError("Gender cannot be Empty")
+
+        if gender.strip().lower() not in ["male", "female"]:
+            raise ValueError("Gender must be either Male or Female. ")
+
+        self.__gender = gender.strip().lower()
+
+
+
+    def set_phone_number(self,phone_number):
+        if len(phone_number) != 11 or not phone_number.isdigit():
+            raise ValueError("Phone number must be 11 digits")
+
+        self.__phone_number = phone_number
+
+    def set_email(self, email: str) -> None:
+        if "@" not in email or not email.strip():
+            raise ValueError("Invalid email address")
+        self.__email = email
---
+++ StudentManagement/test/student_management_system/test_student_creation.py
@@ -0,0 +1,47 @@
+from unittest import TestCase
+
+from src.student_management_system.student_creation import Student
+
+
+class StudentTest(TestCase):
+
+    def setUp(self):
+        self._student =  Student("Afolabi David",13,"09071151567","MALe", "afolabi@gmail.com")
+
+
+    def test_student_is_craeted_with_all_the_right_credentials(self):
+
+        self.assertEqual("Afolabi David",self._student.get_name())
+        self.assertEqual(13, self._student.get_age())
+        self.assertEqual("09071151567",self._student.get_phone_number())
+        self.assertEqual("male",self._student.get_gender())
+        self.assertEqual("afolabi@gmail.com",self._student.get_email())
+
+
+    def test_after_student_creation_the_student_is_increases (self):
+
+        self.assertEqual(0o01, self._student.get_student_id())
+        self._student =  Student("Olidi Dave",13,"08071151567","MALE", "afolabi@123gmail.com")
+        self.assertEqual(0o02, self._student.get_student_id())
+
+
+    def test_that_empty_name_will_throw_error_and_not_create_student(self):
+        self._student2 = None
+
+        with self.assertRaises(ValueError):
+            self._student2 = Student("", 13, "08071151567", "MALE", "afolabi@123gmail.com")
+
+        self.assertIsNone(self._student2)
+
+    def test_that_gender_is_either_male_or_female(self):
+
+        self._student2= None
+
+        with self.assertRaises(ValueError):
+            self._student2 = Student("Olidi Dave",13,"08071151567","man", "afolabi@123gmail.com")
+
+        self.assertIsNone(self._student2)
+
+    def test_that_student_age_cannot_be_negative(self):
+        with self.assertRaises(ValueError):
+            self._student.set_age(-1)
